36f5ab6d0374df5d1fde526667e9e970ea5de676
chenjian <1766446438@qq.com>
2019-09-24 17:29:31 +0800

    添加，展示功能路由跳转都已经实现，数据未上传到自己的数据库

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,19 @@
 import BlogHead from '@/components/BlogHead'
 import AddBlog from '@/components/AddBlog'
 import ShowBlog from '@/components/ShowBlog'
+<<<<<<< HEAD
 import Game from '@/components/Game'
+=======
+>>>>>>> 添加，展示功能路由跳转都已经实现，数据未上传到自己的数据库
 
 export default {
   name: 'App',
   components:{
+<<<<<<< HEAD
     BlogHead,AddBlog,ShowBlog,Game
+=======
+    BlogHead,AddBlog,ShowBlog
+>>>>>>> 添加，展示功能路由跳转都已经实现，数据未上传到自己的数据库
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,10 @@ import Router from 'vue-router'
 import AddBlog from '@/components/AddBlog'
 import ShowBlog from '@/components/ShowBlog'
 import SingleBlog from '@/components/SingleBlog'
+<<<<<<< HEAD
 import Game from '@/components/Game'
+=======
+>>>>>>> 添加，展示功能路由跳转都已经实现，数据未上传到自己的数据库
 
 Vue.use(Router)
 // 加上mode:"history"之后可以去掉URL里面的锚点
@@ -22,11 +25,14 @@ export default new Router({
       component: ShowBlog
     },
     {
+<<<<<<< HEAD
       path: '/game',
       name: 'Game',
       component: Game
     },
     {
+=======
+>>>>>>> 添加，展示功能路由跳转都已经实现，数据未上传到自己的数据库
       path: '/blog/:id',
       name: 'SingleBlog',
       component: SingleBlog
